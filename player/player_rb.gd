6495ae6ae35b19e1788e5a64ff2989157ef5f1b4
Connor <61102529+Metater@users.noreply.github.com>
2023-09-23 17:03:58 -0400

    Revamp player

--- player/player_rb.gd
+++ player/player_rb.gd
@@ -11,19 +11,73 @@ var desired_heading = current_heading
 var last_turn_direction = 1 # turn CCW by default
 var input_physics_frame_count = 0
 
-func _ready():
-	position = Vector2(200, 200)
-	
-	$"../Sprite2D".reset()
-
 func _integrate_forces(state):
-	rotation = sin(Time.get_ticks_msec() / 1000.0)
+	var delta = state.step
+	
+	update_heading(delta)
+	update_linear_velocity(state, delta)
+	
+	$"../Sprite2D".index_pose(position, current_heading)
 
+func update_heading(delta):
+	update_desired_heading()
+	# the most the heading could change this frame, radians
+	var max_heading_delta = rotation_speed * delta;
+	# the unit circle arc distance from current_heading to desired_heading, radians
+	var heading_error = Vector2.from_angle(current_heading).angle_to(Vector2.from_angle(desired_heading))
+	if heading_error == 0:
+		pass
+	elif abs(heading_error) <= max_heading_delta:
+		current_heading = desired_heading
+		last_turn_direction = binary_sign(heading_error)
+	else:
+		var turn_direction = binary_sign(heading_error)
+		# if starting a 180, turn in the direction of the last turn
+		if abs(heading_error) > (0.99999 * PI) && abs(heading_error) < (1.00001 * PI):
+			turn_direction = -last_turn_direction
+		# turn by max increment in the specified direction
+		current_heading += max_heading_delta * turn_direction
+		last_turn_direction = turn_direction
+func update_desired_heading():
+	var vector = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		vector.x -= 1
+	if Input.is_action_pressed("move_right"):
+		vector.x += 1
+	if Input.is_action_pressed("move_down"):
+		vector.y -= 1
+	if Input.is_action_pressed("move_up"):
+		vector.y += 1
+	
+	if vector.length() > 0:
+		vector = vector.normalized()
+		desired_heading = vector.angle()
+		input_physics_frame_count += 1
+	else:
+		input_physics_frame_count = 0
+func binary_sign(value):
+	if value >= 0.0:
+		return 1.0
+	return -1.0
+
+func update_linear_velocity(state, delta):
+	# dot product of current_heading and desired_heading, range: [-1, 1]
+	var velocity_fraction = Vector2.from_angle(current_heading).dot(Vector2.from_angle(desired_heading))
+	
+	# set movement_vector to current_heading vector with y flipped
+	var movement_vector = Vector2.from_angle(current_heading) * Vector2(1, -1)
+	var input_accel_fraction = get_input_accel_fraction(delta)
+	var desired_linear_velocity = movement_vector * (forward_speed * velocity_fraction * input_accel_fraction)
+	if input_physics_frame_count == 0:
+		desired_linear_velocity = Vector2.ZERO
+	
+	# accelerate and apply
+	var desired_linear_velocity_x = move_toward(linear_velocity.x, desired_linear_velocity.x, forward_accel * delta)
+	var desired_linear_velocity_y = move_toward(linear_velocity.y, desired_linear_velocity.y, forward_accel * delta)
+	state.linear_velocity = Vector2(desired_linear_velocity_x, desired_linear_velocity_y)
 func get_input_accel_fraction(delta):
 	var seconds_since_input_began = input_physics_frame_count * delta
 	var t = seconds_since_input_began / input_accel_time
 	t = clampf(t, 0, 1)
 	return sqrt(t)
-
-func teleport(position, rotation):
-	
--- player/player_sprite.gd
+++ player/player_sprite.gd
@@ -2,25 +2,27 @@ extends Sprite2D
 
 var position_0: Vector2
 var position_1: Vector2
-
 var rotation_0: float
 var rotation_1: float
 
-func _process(delta):
+func _process(_delta):
 	var t = Engine.get_physics_interpolation_fraction()
+	
 	position = position_0.lerp(position_1, t)
-	rotation = lerp_angle(rotation_0, rotation_1, t)
-
-func reset():
-	position_0 = $"../RigidBody2D".position
-	position_1 = $"../RigidBody2D".position
 	
-	rotation_0 = $"../RigidBody2D".rotation
-	rotation_1 = $"../RigidBody2D".rotation
+	var desired_rotation = lerp_angle(rotation_0, rotation_1, t)
+	# the given rotation is unit circle based
+	desired_rotation = (-desired_rotation) - PI / 2;
+	rotation = desired_rotation
 
-func index(position, rotation):
+func set_pose(desired_position, desired_rotation):
+	position_0 = desired_position
+	position_1 = desired_position
+	rotation_0 = desired_rotation
+	rotation_1 = desired_rotation
+
+func index_pose(desired_position, desired_rotation):
 	position_0 = position_1
-	position_1 = position
-	
+	position_1 = desired_position
 	rotation_0 = rotation_1
-	rotation_1 = rotation
+	rotation_1 = desired_rotation
