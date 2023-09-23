04c1b4cf59a6e099ef345be9f960ec895c18e658
Connor <61102529+Metater@users.noreply.github.com>
2023-09-23 12:53:55 -0400

    Save

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+@export var spawn_position: Vector2
+
 const rotation_speed = 1.5 * PI # radians / sec
 const forward_speed = 800 # pixels / sec
 const accel_time = 0.5 # sec
@@ -11,90 +13,29 @@ var desired_heading = current_heading
 var last_turn_direction = 1 # turn CCW by default
 var input_physics_frame_count = 0
 
+var position_0: Vector2
+var position_1: Vector2
+var rotation_0: float
+var rotation_1: float
+
 func _ready():
-	position = Vector2(200, 200)
-
-func _physics_process(delta):
-#	print(Engine.get_physics_interpolation_fraction())
-	update_heading(delta)
-	update_linear_velocity(delta)
-
-func update_heading(delta):
-	update_desired_heading()
-	# the most the heading could change this frame, radians
-	var max_heading_delta = rotation_speed * delta;
-	# the unit circle arc distance from current_heading to desired_heading, radians
-	var heading_error = Vector2.from_angle(current_heading).angle_to(Vector2.from_angle(desired_heading))
-	if heading_error == 0:
-		pass
-	elif abs(heading_error) <= max_heading_delta:
-		current_heading = desired_heading
-		last_turn_direction = binary_sign(heading_error)
-	else:
-		var turn_direction = binary_sign(heading_error)
-		# if starting a 180, turn in the direction of the last turn
-		if abs(heading_error) > (0.99999 * PI) && abs(heading_error) < (1.00001 * PI):
-			turn_direction = last_turn_direction
-		# turn by max increment in the specified direction
-		current_heading += max_heading_delta * turn_direction
-		last_turn_direction = turn_direction
-	set_heading(current_heading)
-
-func update_desired_heading():
-	var vector = Vector2.ZERO
+	position = spawn_position
 	
-	if Input.is_action_pressed("move_left"):
-		vector.x -= 1
-	if Input.is_action_pressed("move_right"):
-		vector.x += 1
-	
-	if Input.is_action_pressed("move_down"):
-		vector.y -= 1
-	if Input.is_action_pressed("move_up"):
-		vector.y += 1
-	
-	if vector.length() > 0:
-		vector = vector.normalized()
-		desired_heading = vector.angle()
-		input_physics_frame_count += 1
-	else:
-		input_physics_frame_count = 0
+	position_0 = position
+	position_1 = position
+	rotation_0 = rotation
+	rotation_1 = rotation
 
-func set_heading(heading):
-	# convert unit circle rotation to godot's rotation
-	heading = -heading;
-	heading -= PI / 2
-	# apply
-	$Sprite2D.rotation = heading
-	$CollisionShape2D.rotation = heading
+func _process(delta):
+	var t = Engine.get_physics_interpolation_fraction()
+	$"../Sprite2D".position = position_0.lerp(position_1, t)
+	$"../Sprite2D".rotation = lerp_angle(rotation_0, rotation_1, t)
 
-func binary_sign(value):
-	if value >= 0.0:
-		return 1.0
-	return -1.0
-
-func update_linear_velocity(delta):
-	# dot product of current_heading and desired_heading, range: [-1, 1]
-	var heading_error_dot = Vector2.from_angle(current_heading).dot(Vector2.from_angle(desired_heading))
-	# maps heading_error_dot -> velocity_multiplier: [-1, 0) -> [-0.5, 0); [0, 1] -> [0, 1]
-	# var velocity_multiplier = heading_error_dot if heading_error_dot >= 0 else heading_error_dot / 2.0
-	# maps heading_error_dot -> velocity_multiplier: [-1, 0) -> 0; [0, 1] -> [0, 1]
-	# var velocity_multiplier = heading_error_dot if heading_error_dot >= 0 else 0
-	var velocity_multiplier = heading_error_dot
+func _integrate_forces(state):
+	state.linear_velocity = Vector2(240, 0)
+	state.angular_velocity = 2 * PI
 	
-	# set movement_vector to current_heading vector with y flipped
-	var movement_vector = Vector2.from_angle(current_heading) * Vector2(1, -1)
-	var input_accel_multiplier = get_input_accel_multiplier(delta)
-	var desired_linear_velocity = movement_vector * (forward_speed * velocity_multiplier * input_accel_multiplier)
-	if input_physics_frame_count == 0:
-		desired_linear_velocity = Vector2.ZERO
-	
-	var desired_linear_velocity_x = move_toward(linear_velocity.x, desired_linear_velocity.x, forward_accel * delta)
-	var desired_linear_velocity_y = move_toward(linear_velocity.y, desired_linear_velocity.y, forward_accel * delta)
-	linear_velocity = Vector2(desired_linear_velocity_x, desired_linear_velocity_y)
-
-func get_input_accel_multiplier(delta):
-	var seconds_since_input_began = input_physics_frame_count * delta
-	var t = seconds_since_input_began / input_accel_time
-	t = clampf(t, 0, 1)
-	return sqrt(t)
+	position_0 = position_1
+	position_1 = position
+	rotation_0 = rotation_1
+	rotation_1 = rotation
