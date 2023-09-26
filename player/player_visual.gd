87912d7d84589c5d0f2ffe779ea8aa99c49aac56
Connor <61102529+Metater@users.noreply.github.com>
2023-09-25 20:48:35 -0400

    Remove barrel visual lerping

--- player/player_visual.gd
+++ player/player_visual.gd
@@ -5,13 +5,9 @@ var body_position_1: Vector2
 var body_rotation_0: float
 var body_rotation_1: float
 
-var barrel_rotation_0: float
-var barrel_rotation_1: float
-
 func _process(_delta):
 	var t = Engine.get_physics_interpolation_fraction()
 	body(t)
-	barrel(t)
 
 func body(t):
 	position = body_position_0.lerp(body_position_1, t)
@@ -30,15 +26,3 @@ func index_body_pose(desired_position, desired_rotation):
 	body_position_1 = desired_position
 	body_rotation_0 = body_rotation_1
 	body_rotation_1 = desired_rotation
-
-func barrel(t):
-	var desired_rotation = lerp_angle(barrel_rotation_0, barrel_rotation_1, t)
-	# the given rotation is unit circle based
-	desired_rotation = (-desired_rotation) - PI / 2;
-	$"BarrelSprite2D".global_rotation = desired_rotation
-func set_barrel_rotation(desired_rotation):
-	barrel_rotation_0 = desired_rotation
-	barrel_rotation_1 = desired_rotation
-func index_barrel_rotation(desired_rotation):
-	barrel_rotation_0 = barrel_rotation_1
-	barrel_rotation_1 = desired_rotation
