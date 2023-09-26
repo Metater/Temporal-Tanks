2d0bd69ed894dc42a114369732edb610cf5af997
Connor <61102529+Metater@users.noreply.github.com>
2023-09-25 20:45:55 -0400

    Refactor and add barrel aiming

--- player/player_rb.gd
+++ player/player_rb.gd
@@ -6,19 +6,24 @@ const body_accel_time = 0.5 # sec
 const body_accel = body_speed / body_accel_time # pixels / sec^2
 const input_accel_time = 0.5 # sec
 
-var current_rotation = PI / 2 # face up by default
-var desired_rotation = current_rotation
-var last_turn_direction = 1 # turn CCW by default, -1 or 1
+var body_current_rotation = PI / 2 # face up by default
+var body_desired_rotation = body_current_rotation
+var last_body_turn_direction = 1 # turn CCW by default, -1 or 1
 var input_physics_frame_count = 0
 
 func _ready():
-	$"../BodySprite2D".set_body_pose(position, current_rotation)
+	$"../BodySprite2D".set_body_pose(position, body_current_rotation)
+
+func _process(delta):
+	# find unit circle angle to point barrel towards mouse
+	var barrel_desired_rotation = ((get_global_mouse_position() - position).normalized() * Vector2(1, -1)).angle()
+	$"../BodySprite2D".set_barrel_rotation(barrel_desired_rotation)
 
 func _physics_process(delta):
-	update_rotation(delta)
+	update_body_rotation(delta)
 	update_linear_velocity(delta)
 	
-	$"../BodySprite2D".index_body_pose(position, current_rotation)
+	$"../BodySprite2D".index_body_pose(position, body_current_rotation)
 
 func _integrate_forces(_state):
 	# must use this, not RigidBody2D.lock_rotation
@@ -26,26 +31,26 @@ func _integrate_forces(_state):
 	# trying accelerating from zero at an angle towards the object
 	rotation = 0
 
-func update_rotation(delta):
-	update_desired_rotation()
+func update_body_rotation(delta):
+	update_body_desired_rotation()
 	# the most the rotation could change this frame, radians
 	var max_rotation_delta = body_rotation_speed * delta;
-	# the unit circle arc distance from current_rotation to desired_rotation, radians
-	var rotation_error = Vector2.from_angle(current_rotation).angle_to(Vector2.from_angle(desired_rotation))
+	# the unit circle arc distance from body_current_rotation to body_desired_rotation, radians
+	var rotation_error = Vector2.from_angle(body_current_rotation).angle_to(Vector2.from_angle(body_desired_rotation))
 	if rotation_error == 0:
 		pass
 	elif abs(rotation_error) <= max_rotation_delta:
-		current_rotation = desired_rotation
-		last_turn_direction = binary_sign(rotation_error)
+		body_current_rotation = body_desired_rotation
+		last_body_turn_direction = binary_sign(rotation_error)
 	else:
 		var turn_direction = binary_sign(rotation_error)
 		# if starting a 180, turn in the direction of the last turn
 		if abs(rotation_error) > (0.99 * PI) && abs(rotation_error) < (1.01 * PI):
-			turn_direction = last_turn_direction
+			turn_direction = last_body_turn_direction
 		# turn by max increment in the specified direction
-		current_rotation += max_rotation_delta * turn_direction
-		last_turn_direction = turn_direction
-func update_desired_rotation():
+		body_current_rotation += max_rotation_delta * turn_direction
+		last_body_turn_direction = turn_direction
+func update_body_desired_rotation():
 	var vector = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_left"):
@@ -59,7 +64,7 @@ func update_desired_rotation():
 	
 	if vector.length() > 0:
 		vector = vector.normalized()
-		desired_rotation = vector.angle()
+		body_desired_rotation = vector.angle()
 		input_physics_frame_count += 1
 	else:
 		input_physics_frame_count = 0
@@ -69,11 +74,11 @@ func binary_sign(value):
 	return -1.0
 
 func update_linear_velocity(delta):
-	# dot product of current_rotation and desired_rotation, range: [-1, 1]
-	var velocity_fraction = Vector2.from_angle(current_rotation).dot(Vector2.from_angle(desired_rotation))
+	# dot product of body_current_rotation and body_desired_rotation, range: [-1, 1]
+	var velocity_fraction = Vector2.from_angle(body_current_rotation).dot(Vector2.from_angle(body_desired_rotation))
 	
-	# set movement_vector to current_rotation vector with y flipped
-	var movement_vector = Vector2.from_angle(current_rotation) * Vector2(1, -1)
+	# set movement_vector to body_current_rotation vector with y flipped
+	var movement_vector = Vector2.from_angle(body_current_rotation) * Vector2(1, -1)
 	var input_accel_fraction = get_input_accel_fraction(delta)
 	var desired = movement_vector * (body_speed * velocity_fraction * input_accel_fraction)
 	if input_physics_frame_count == 0:
